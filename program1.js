3f90335128713c1f07a41bd6a30dbbb027097763
patidarji2001 <153583542+patidarji2001@users.noreply.github.com>
2024-01-05 17:31:23 +0530

    Update program1.js

--- program1.js
+++ program1.js
@@ -3,7 +3,24 @@
  * @return {boolean}
  */
 var isValid = function(s) {
-    
+    const stack=[];
+    const parenthesis={
+    ')':'(',
+        '}':'{',
+        ']':'['
+    };
+    for(let i=0;i<s.length;i++){
+     const char=s[i];
+        if(char ==='(' || char === '{' ||char ==='['){
+            stack.push(char);
+        }
+        else{
+            const corres=parenthesis[char];
+            if(stack.pop() !== corres){
+                return false;
+            }
+        }
+    }
 };
 
 module.exports = { isValid };
